6c91b1c00cf0f3502cbec0f9e97b724a2049e8ad
DanilaMyl <DanilaMylnikov1806@mail.ru>
2023-09-02 01:19:43 +0300

    fix *.sh

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 apt update
-sleep 8
 apt install mongodb -y
-sleep 8
 systemctl start mongodb
-sleep 8
 systemctl enable mongodb
-sleep 8
--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 apt update
-sleep 8
 apt install -y ruby-full ruby-bundler build-essential
-sleep 8
